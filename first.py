e73952137fd97a59dd2f2cc6c169d53f4000d5c0
andrew <andrewradke8@gmail.com>
2023-01-17 19:26:00 -0500

    bruh

--- first.py
+++ first.py
@@ -1,10 +1,4 @@
-print("hi joshyyy! slay queen")
-print("we are winning this shit")
-for i in range(10):
-    print("Hi Beliz")
-
-    #can we kick beliz out of the group?
-
-while(1):
-    print("siktir git")
+sahil = 5
+sahil = 5 + 2.3
+print(sahil)
 
