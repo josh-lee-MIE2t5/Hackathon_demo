6d369001b4b5851932e70be6f162853af076e5dc
bzorbozan <beliz.zorbozan@gmail.com>
2023-01-17 19:20:08 -0500

    i added a random ass java file bc why not bitches

---
+++ First.java
@@ -0,0 +1,9 @@
+class First {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10) {
+        }
+    }
+    // shift option f to format
+    // command shift p to search for any command.
+}
