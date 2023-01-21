f9bf1d573f556cf8929371bbdc8d18705e82f4ae
s-v-amin <sahilaminbisak@gmail.com>
2023-01-21 07:49:09 -0500

    updated_webscrape_finder

--- webscrape_sahil.py
+++ webscrape_sahil.py
@@ -1,15 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
-baseurl = 'https://www.toronto.ca/'
+baseurl = 'https://www.destinationtoronto.com/'
 
 headers = {
     'User-Agent': 'https://developers.whatismybrowser.com/useragents/parse/?analyse-my-user-agent=yes'
 }
 
-r = requests.get('https://www.toronto.ca/explore-enjoy/festivals-events/festivals-events-calendar/')
+r = requests.get('https://www.destinationtoronto.com/events/?view=list&sort=date&bounds=false')
 soup = BeautifulSoup(r.content, 'lxml')
 
-eventsList = soup.find_all('div', class_="row event-list-row")
+i = 0
 
-print(eventsList)
+tabContent = soup.find_all('div', class_="root-panel panel-page")
+print(tabContent)
+
+# eventsList = soup.find_all('div', class_="row event-list-row", recursive= False)
+# print(event)
+# print(eventNew)
+# for item in eventsList:
+#      for display in item.find_all('div', id_="fecListDisplay"):
+#         print(display)
