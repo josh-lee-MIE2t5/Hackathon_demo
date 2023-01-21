4500097909f0021e090175306065f79dc26968f5
s-v-amin <sahilaminbisak@gmail.com>
2023-01-21 10:38:08 -0500

    eventbrite push

--- webscrape_sahil.py
+++ webscrape_sahil.py
@@ -1,22 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
-baseurl = 'https://seatgeek.com/'
+baseurl = 'https://www.eventbrite.ca/'
 
 headers = {
     'User-Agent': 'https://developers.whatismybrowser.com/useragents/parse/?analyse-my-user-agent=yes'
 }
 
-r = requests.get('https://seatgeek.com/cities/toronto')
+r = requests.get('https://www.eventbrite.ca/d/canada--toronto/events/', headers= headers)
 soup = BeautifulSoup(r.content, 'lxml')
 
 eventLinks = []
-tabContent = soup.find_all('div', class_="small-card-mobile")
+tabContent = soup.find_all('div', class_="eds-event-card-content__primary-content")
 for item in tabContent:
     for link in item.find_all('a', href=True):
-        eventLinks.append(baseurl + link['href'])
+        eventLinks.append(link['href'])
 
 print(eventLinks)
+
 
 
 
